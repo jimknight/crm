db902a7284fd67e03fd78aa93fd875e9317f19e2
Jim Knight <jim.knight@lavatech.com>
2014-10-31 12:16:04 -0400

    ordering lists

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -4,7 +4,7 @@ class IndustriesController < ApplicationController
   # GET /industries
   # GET /industries.json
   def index
-    @industries = Industry.all
+    @industries = Industry.all.order("name")
   end
 
   # GET /industries/1
