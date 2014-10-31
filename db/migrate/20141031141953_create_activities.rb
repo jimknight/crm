b26547766a7b0e4bad2411fc494e15a804a9c7d4
Jim Knight <jim.knight@lavatech.com>
2014-10-31 10:25:14 -0400

    fixed activities migration and set default route

--- db/migrate/20141031141953_create_activities.rb
+++ db/migrate/20141031141953_create_activities.rb
@@ -2,7 +2,7 @@ class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities do |t|
       t.belongs_to :client, index: true
-      t.date_time :activity_date
+      t.datetime :activity_date
       t.belongs_to :contact, index: true
       t.string :city
       t.string :state
---
+++ db/schema.rb
@@ -0,0 +1,77 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141031142042) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "activities", force: true do |t|
+    t.integer  "client_id"
+    t.datetime "activity_date"
+    t.integer  "contact_id"
+    t.string   "city"
+    t.string   "state"
+    t.string   "industry"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["client_id"], name: "index_activities_on_client_id", using: :btree
+  add_index "activities", ["contact_id"], name: "index_activities_on_contact_id", using: :btree
+
+  create_table "activities_models", id: false, force: true do |t|
+    t.integer "activity_id", null: false
+    t.integer "model_id",    null: false
+  end
+
+  create_table "clients", force: true do |t|
+    t.string   "name"
+    t.string   "street1"
+    t.string   "street2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "industry"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "email"
+    t.string   "work_phone"
+    t.string   "mobile_phone"
+    t.integer  "client_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contacts", ["client_id"], name: "index_contacts_on_client_id", using: :btree
+
+  create_table "industries", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "models", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
