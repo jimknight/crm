f6f42e83b9868f5638b976e8e7ccc7f36104f6e2
Jim Knight <jim.knight@lavatech.com>
2014-11-29 16:43:52 -0500

    timing issues in tests but working

--- spec/features/clients_spec.rb
+++ spec/features/clients_spec.rb
@@ -2,13 +2,15 @@ require "rails_helper"
 
 describe "show" do
   it "should allow creation of a contact" do
-    @client = Client.create!(:name => "SGA")
     visit new_user_registration_path
     fill_in "Email", :with => "wscarano@sga.com"
     fill_in "Password", :with => "ilovesga"
     fill_in "Password confirmation", :with => "ilovesga"
     click_button "Sign up"
-    visit client_path(@client)
+    click_link "Clients"
+    click_link "New Client"
+    fill_in "Name", :with => "Wayne Scarano"
+    click_button "Create Client"
     click_link "Add contact"
     fill_in "Name", :with => "Wayne Scarano"
     click_button "Create Contact"
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "User" do
+  it "should attach the activity to the current user", :js => true do
+    # User.destroy_all
+    # @client = Client.create!(:name => "SGA", :city => "Hillsborough", :state => "NJ")
+    # @contact = Contact.create!(:name => "Wayne Scarano")
+    # @client.contacts << @contact
+    # @user = User.create!(:email => "wscarano@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga")
+    # @user.activities.count.should == 0
+    # visit new_activity_path
+    # fill_in "Email", :with => "wscarano@sga.com"
+    # fill_in "Password", :with => "ilovesga"
+    # click_button "Sign in"
+    # select "SGA", :from => "Client"
+    # select "Wayne Scarano", :from => "Contact"
+    # fill_in "Comments", :with => "i am wayne"
+    # click_button "Save"
+    # visit
+  end
+end
