4743a39aab9cb1eeb26ed09a654162d3ae3f330b
Jim Knight <jim.knight@lavatech.com>
2014-12-03 18:38:18 -0500

    added the admin ability to modify industries

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -35,6 +35,7 @@ describe "Users" do
     page.should have_content "i am wayne"
   end
   it "who are admins can Create/Edit/Read all Models" do
+    User.destroy_all
     @admin = User.create!(:email => "admin@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga", :admin => true)
     visit new_model_path
     fill_in "Email", :with => "admin@sga.com"
@@ -47,5 +48,20 @@ describe "Users" do
     fill_in "Name", :with => "Model1Updated"
     click_button "Save"
     page.should have_link "Model1Updated"
+  end
+  it "who are admins can Create/Edit/Read all Industries" do
+    User.destroy_all
+    @admin = User.create!(:email => "admin@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga", :admin => true)
+    visit new_industry_path
+    fill_in "Email", :with => "admin@sga.com"
+    fill_in "Password", :with => "ilovesga"
+    click_button "Sign in"
+    fill_in "Name", :with => "Industry1"
+    click_button "Save"
+    click_link "Industry1"
+    click_link "Edit"
+    fill_in "Name", :with => "Industry1Updated"
+    click_button "Save"
+    page.should have_link "Industry1Updated"
   end
 end
