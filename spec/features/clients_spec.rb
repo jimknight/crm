d11566a5921a7472d7b007e32b4deadbfe44de3a
Jim Knight <jim.knight@lavatech.com>
2014-12-06 15:21:20 -0500

    Got tests a little more accurate for clients

--- spec/features/clients_spec.rb
+++ spec/features/clients_spec.rb
@@ -2,18 +2,19 @@ require "rails_helper"
 
 describe "show" do
   it "should allow creation of a contact" do
-    visit new_user_registration_path
-    fill_in "Email", :with => "wscarano@sga.com"
+    User.destroy_all
+    @jim = User.create!(:email => "jknight@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga")
+    visit new_activity_path
+    fill_in "Email", :with => "jknight@sga.com"
     fill_in "Password", :with => "ilovesga"
-    fill_in "Password confirmation", :with => "ilovesga"
-    click_button "Sign up"
+    click_button "Sign in"
     click_link "Clients"
     click_link "New Client"
     fill_in "Name", :with => "Wayne Scarano"
-    click_button "Create Client"
+    click_button "Save"
     click_link "Add contact"
     fill_in "Name", :with => "Wayne Scarano"
-    click_button "Create Contact"
+    click_button "Save"
     click_link "Wayne Scarano"
   end
 end
