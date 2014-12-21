4d96b91911d3e0370ba563b0c03f28d4587074b6
Jim Knight <jim.knight@lavatech.com>
2014-12-21 17:17:58 -0500

    setup nginx settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,15 @@ set :deploy_to, "/home/sgadeploy/crm"
 
 namespace :deploy do
 
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
