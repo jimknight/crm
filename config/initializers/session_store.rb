8ad00b23c2cfa963b9bf69ccd3e4959a6d39f8aa
Jim Knight <jim.knight@lavatech.com>
2015-01-02 09:19:48 -0500

    trying to fix a production error
    I, [2015-01-01T06:22:50.276484 #17759]  INFO -- : Started GET "/" for 62.75.216.19 at 2015-01-01 06:22:50 -0500
    I, [2015-01-01T06:22:50.279992 #17759]  INFO -- : Processing by ActivitiesController#index as */*
    I, [2015-01-01T06:22:50.281271 #17759]  INFO -- : Completed 401 Unauthorized in 1ms

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_crm_session'
+# Rails.application.config.session_store :cookie_store, key: '_crm_session'
+
+# http://stackoverflow.com/questions/19168178/rails-401-unauthorized-when-i-access-action-in-production-only
+Rails.application.config.session_store :cookie_store, key: '_crm_session', domain: {
+  production: 'crm.sga.com',
+  development: 'development_domain'
+}.fetch(Rails.env.to_sym, :all)
