93f1f95133a30bfda03728b0fd74f864afbdce1f
Jim Knight <jim.knight@lavatech.com>
2015-03-28 14:42:03 -0400

    only show the preview if it's an image

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,10 @@
 module ActivitiesHelper
+
+  def attachment_is_image?
+    if @activity.attachment_url.present?
+      return /(.+\.(?:png|jpg|gif|jpeg|icon))/i.match(@activity.attachment_url)
+    end
+    return false
+  end
+
 end
