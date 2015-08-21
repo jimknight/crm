16fe32681889f0132a916d06d92ffad8b8023807
Jim Knight <jim.knight@lavatech.com>
2015-08-21 12:07:04 -0400

    added list of ram's to show in client

--- spec/features/clients_spec.rb
+++ spec/features/clients_spec.rb
@@ -20,6 +20,17 @@ describe "show" do
     click_button "Save"
     click_link "Wayne Scarano"
   end
+  it "should show reps(rsm's) associated" do
+    @rep_for_client = User.create!(:email => "rep_for_client@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga")
+    @rep_wo_client = User.create!(:email => "rep_wo_client@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga")
+    @client_reps = Client.create!(:name => "YesReps")
+    @client_reps.users << @rep_for_client
+    visit client_path(@client_reps)
+    fill_in "Email", :with => "rep_wo_client@sga.com"
+    fill_in "Password", :with => "ilovesga"
+    click_button "Sign in"
+    page.should have_content @rep_for_client.user_name
+  end
 end
 describe "index" do
   it "should only show clients to specific reps (rsm's) and admins" do
