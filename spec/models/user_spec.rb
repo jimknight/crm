81e1b92ee4e9a3338d159149e76edd6374c87b44
Jim Knight <jim.knight@lavatech.com>
2015-10-30 12:13:59 -0400

    Added a role to user for marketing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@
 #  created_at             :datetime
 #  updated_at             :datetime
 #  admin                  :boolean          default(FALSE)
+#  role                   :string(255)
 #
 
 require 'rails_helper'
