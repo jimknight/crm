00296069e19f5751f603fce78a2fbebdb48402d0
Jim Knight <jim.knight@lavatech.com>
2015-11-12 14:06:26 -0500

    make the email address in contact a hot spot

--- spec/features/contacts_spec.rb
+++ spec/features/contacts_spec.rb
@@ -14,3 +14,19 @@ describe "new" do
     find_field('Work phone').value.should eq '908-359-4626'
   end
 end
+
+describe "show" do
+  it "should have a link to the client email" do
+    User.destroy_all
+    @client = Client.create!(:name => "SGA", :phone => "908-359-4626")
+    @contact = Contact.create!(:name => "Wayne Scarano", :email => "wscarano@sga.com")
+    @user = User.create!(:email => "user@sga.com", :password => "ilovesga", :password_confirmation => "ilovesga")
+    @user.clients << @client
+    @client.contacts << @contact
+    visit client_contact_path(@client,@contact)
+    fill_in "Email", :with => "user@sga.com"
+    fill_in "Password", :with => "ilovesga"
+    click_button "Sign in"
+    page.should have_link "wscarano@sga.com"
+  end
+end
