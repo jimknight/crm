a0cb41043061f06bc0ef68a08d2671da355855d6
Jim Knight <jim.knight@lavatech.com>
2015-11-29 14:03:45 -0500

    try try again

--- config/deploy.rb
+++ config/deploy.rb
@@ -131,6 +131,16 @@ namespace :deploy do
       execute "rm -rf #{release_path}/log"
       execute "ln -s  #{shared_path}/log #{release_path}"
 
+      # Whenever
+      set :whenever_roles,        ->{ :db }
+      set :whenever_command,      ->{ [:bundle, :exec, :whenever] }
+      set :whenever_command_environment_variables, ->{ {} }
+      set :whenever_identifier,   ->{ fetch :application }
+      set :whenever_environment,  ->{ fetch :rails_env, fetch(:stage, "production") }
+      set :whenever_variables,    ->{ "environment=#{fetch :whenever_environment}" }
+      set :whenever_update_flags, ->{ "--update-crontab #{fetch :whenever_identifier} --set #{fetch :whenever_variables}" }
+      set :whenever_clear_flags,  ->{ "--clear-crontab #{fetch :whenever_identifier}" }
+
     end
   end
 
