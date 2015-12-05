6330e665c2d0e0362dfec8dacd29f8ab83bd10de
Jim Knight <jim.knight@lavatech.com>
2015-12-05 15:06:39 -0500

    annotate

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -2,21 +2,26 @@
 #
 # Table name: clients
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  street1     :string(255)
-#  street2     :string(255)
-#  city        :string(255)
-#  state       :string(255)
-#  zip         :string(255)
-#  phone       :string(255)
-#  industry    :string(255)
-#  created_at  :datetime
-#  updated_at  :datetime
-#  fax         :string(255)
-#  street3     :string(255)
-#  client_type :string(255)
-#  status      :text
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  street1         :string(255)
+#  street2         :string(255)
+#  city            :string(255)
+#  state           :string(255)
+#  zip             :string(255)
+#  phone           :string(255)
+#  industry        :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  fax             :string(255)
+#  street3         :string(255)
+#  client_type     :string(255)
+#  status          :text
+#  eid             :string(255)
+#  prospect_type   :text
+#  source          :text
+#  form_dump       :text
+#  import_datetime :datetime
 #
 
 require 'rails_helper'
