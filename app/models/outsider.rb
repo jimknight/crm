7f59d98f23935d504cac7bf29d4ecbcd33a70658
Jim Knight <jim.knight@lavatech.com>
2016-02-22 17:18:05 -0500

    annotate

--- app/models/outsider.rb
+++ app/models/outsider.rb
@@ -3,9 +3,9 @@
 # Table name: outsiders
 #
 #  id         :integer          not null, primary key
-#  email      :string(255)
-#  first_name :string(255)
-#  last_name  :string(255)
+#  email      :string
+#  first_name :string
+#  last_name  :string
 #  created_at :datetime
 #  updated_at :datetime
 #
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,8 @@
 # Table name: profiles
 #
 #  id         :integer          not null, primary key
-#  first_name :string(255)
-#  last_name  :string(255)
+#  first_name :string
+#  last_name  :string
 #  user_id    :integer
 #  created_at :datetime
 #  updated_at :datetime
