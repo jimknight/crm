a9145d1d97cef1b6af524ecf4a4fcf29fd0f3d99
Jim Knight <jim.knight@lavatech.com>
2016-06-15 19:34:50 -0400

    set the time based on profile or fall to default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  around_filter :user_time_zone, :if => :current_user
+
   # Devise https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-edit-their-password
   before_filter :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
@@ -15,6 +17,14 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin_user!
     raise SecurityError unless current_user.try(:admin?)
+  end
+
+  def user_time_zone(&block)
+    if current_user.profile.time_zone.nil?
+      Time.use_zone(Time.zone, &block)
+    else
+      Time.use_zone(current_user.profile.time_zone, &block)
+    end
   end
 
   rescue_from SecurityError do |exception|
