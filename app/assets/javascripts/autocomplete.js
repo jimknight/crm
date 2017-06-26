fd11c86ac58b1b32f8b8cb1fe039fcbb23c2c990
Jim Knight <jimknight@lavatech.com>
2017-06-26 19:25:23 -0400

    Trying to fix apparent JS not running

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,4 +1,5 @@
-$( document ).ready(function() {
+$(document).on('turbolinks:load', function() {
+  console.log("page loaded");
   $('input#client_name').bind('railsAutocomplete.select', function(event, data){
     getClientJson(data.item.id);
     $("input#activity_client_id").val(data.item.id);
