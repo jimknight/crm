71384160a8e7ef97a005c9a4636152a3ae1385ed
Jim Knight <jim@lavatech.com>
2018-12-25 19:06:06 -0500

    Auto logoff in 30 mins

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_crm_session'
-
 # http://stackoverflow.com/questions/19168178/rails-401-unauthorized-when-i-access-action-in-production-only
-Rails.application.config.session_store :cookie_store, key: '_crm_session', domain: {
-  production: 'crm.rossmixing.com'
-}.fetch(Rails.env.to_sym, :all)
+Rails.application.config.session_store :cookie_store,
+  key: '_crm_session',
+  expire_after: 30.minutes,
+  domain: {production: 'crm.rossmixing.com'}.fetch(Rails.env.to_sym, :all)
