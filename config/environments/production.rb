d213d533e2ac2b60d7b55b9855c1a988a9d132ca
Jim Knight <jim@lavatech.com>
2021-06-14 21:34:35 +0000

    Changed IP to SGA public IP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'https://crm.rossmixing.com' }
-  #config.action_mailer.smtp_settings = {
-  #  :address => "10.0.1.21",
-  #  :openssl_verify_mode  => 'none'
-  #}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: '10.0.1.21',
+    address: '35.169.59.160',
     port: 25,
     domain: 'sga.com'
   }
